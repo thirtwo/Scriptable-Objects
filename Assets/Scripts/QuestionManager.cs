15d9ceedfaeb55139ad9bf073b82f996fea44f9a
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-02-04 22:10:33 +0300

    Scriptable Object
    Scriptable Object main

---
+++ Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionManager : MonoBehaviour
+{
+    [SerializeField] private ScriptableText[] scriptableTexts;
+    [SerializeField] private Text questionText;
+    [SerializeField] private Text answer0Button;
+    [SerializeField] private Text answer1Button;
+    [SerializeField] private GameObject answer0GO;
+    [SerializeField] private GameObject answer1GO;
+    private string question;
+    private string answer0;
+    private string answer1;
+    private int answer0Price;
+    private int answer1Price;
+    public int bonusPrice = 0;
+    [SerializeField] private int Scriptable_Index = 0;
+
+    void Start()
+    {
+        QuestionPrepare();
+    }
+
+    public void NextQuestionButtonLeft()
+    {
+        if (Scriptable_Index < scriptableTexts.Length-1)
+        {
+            bonusPrice += answer1Price;
+            Scriptable_Index++;
+            QuestionPrepare();
+        }
+        else
+        {
+            answer0GO.SetActive(false);
+            answer1GO.SetActive(false);
+            bonusPrice += answer1Price;
+            questionText.text = "You have earned " + bonusPrice + "$";
+        }
+    }
+    public void NextQuestionButtonRight()
+    {
+        if (Scriptable_Index < scriptableTexts.Length-1)
+        {
+            bonusPrice += answer0Price;
+            Scriptable_Index++;
+            QuestionPrepare();
+        }
+        else
+        {
+            answer0GO.SetActive(false);
+            answer1GO.SetActive(false);
+            bonusPrice += answer0Price;
+            questionText.text = "You have earned " + bonusPrice + "$";
+        }
+    }
+
+    private void QuestionPrepare()
+    {
+        question = scriptableTexts[Scriptable_Index].question;
+        answer0 = scriptableTexts[Scriptable_Index].Answer0;
+        answer1 = scriptableTexts[Scriptable_Index].Answer1;
+        answer0Price = scriptableTexts[Scriptable_Index].Answer0Price;
+        answer1Price = scriptableTexts[Scriptable_Index].Answer1Price;
+        questionText.text = question;
+        answer0Button.text = answer0;
+        answer1Button.text = answer1;
+    }
+}
---
+++ Assets/Scripts/ScriptableText.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableText")]
+public class ScriptableText : ScriptableObject
+{
+    [TextArea(4, 4)] [SerializeField] private string questionText;
+    [SerializeField] private string answer0;
+    [SerializeField] private string answer1;
+    [SerializeField] private int answer0Price;
+    [SerializeField] private int answer1Price;
+
+
+    public string question
+    {
+        get
+        {
+            return questionText;
+        }
+    }
+    public string Answer0
+    {
+        get
+        {
+            return answer0;
+        }
+    }
+    public string Answer1
+    {
+        get
+        {
+            return answer1;
+        }
+    }
+    public int Answer0Price
+    {
+        get
+        {
+            return answer0Price;
+        }
+    }
+    public int Answer1Price
+    {
+        get
+        {
+            return answer1Price;
+        }
+    }
+}
