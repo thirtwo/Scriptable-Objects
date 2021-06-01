ef9bae1bc9148c02e3daff349d0780ac31604031
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-06-01 09:23:46 +0300

    Material Property Block added

---
+++ Assets/Scripts/MaterialPropertyBlock/MPBController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MPBController : MonoBehaviour
+{
+    [SerializeField] private Color mainColor;
+    Renderer _renderer;
+    MaterialPropertyBlock _MPB;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>(); // getting renderer of this obj
+        _MPB = new MaterialPropertyBlock(); // instancing new Material Propery Block
+        _MPB.SetColor("_Color", mainColor); // Setting color only this obj. Not creating new material. Because we said that shader only hold color values per obj. Other values same.
+        _renderer.SetPropertyBlock(_MPB); // we setting this Material Propery Block to our obj
+    }
+}
