6930cb4f9b3b3c1f72fa855c91c6ad6ea460621e
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-06-01 15:49:45 +0300

    Delegate method basics added

---
+++ Assets/Scripts/Delegate/CoinController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CoinController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI coinText;
+    private int _coin = 0;
+
+    private delegate void EarnMoneyDelegate();
+    EarnMoneyDelegate earnMoneyDelegate;
+
+    private void Start()
+    {
+        earnMoneyDelegate += Earn50Coin;
+        earnMoneyDelegate += UpdateCoinText;
+    }
+
+    public void EarnMoneyButton()
+    {
+        earnMoneyDelegate();
+    }
+
+    private void Earn50Coin()
+    {
+        _coin += 50;
+    }
+
+    private void UpdateCoinText()
+    {
+        coinText.text = _coin.ToString();
+    }
+}
