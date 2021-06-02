70505274c1831ef550f9c2b6ea7f674f8178c8d5
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-06-02 17:45:49 +0300

    Events added

---
+++ Assets/Scripts/Events/CoinManagerwithEvent.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class CoinManagerwithEvent : MonoBehaviour
+{
+    public static CoinManagerwithEvent instance = null; // Basic singleton
+    public  int coin = 0;
+    public  int earningPrize = 50;
+
+    public delegate void EarnMoneyDelegate(); // we need delegates for events
+    public static event EarnMoneyDelegate EarnMoneyEvent; // Creating events
+    public delegate void ChangePrize();
+    public static event ChangePrize ChangePrizeEvent;
+
+    private void Awake()
+    {
+        instance = this;
+        EarnMoneyEvent += Earn50Coin; // registering with an event
+    }
+
+
+    private void Earn50Coin()
+    {
+        coin += earningPrize;
+    }
+
+    private void Add10CointoPrize()
+    {
+        earningPrize += 10;
+    }
+
+    private void Subract10CoinfromPrize()
+    {
+        earningPrize -= 10;
+    }
+
+    public void EarnMoneyButton()
+    {
+        EarnMoneyEvent?.Invoke(); // Starting EarnMoneyEvent if not null
+    }
+
+    public void AddCointoPrizeButton()
+    {
+        ChangePrizeEvent = null;
+        ChangePrizeEvent += Add10CointoPrize;
+        ChangePrizeEvent += UIManager.instance.UpdatePrizeText;
+        ChangePrizeEvent?.Invoke();
+    }
+
+    public void SubractCointoPrizeButton()
+    {
+        ChangePrizeEvent = null;
+        ChangePrizeEvent += Subract10CoinfromPrize;
+        ChangePrizeEvent += UIManager.instance.UpdatePrizeText;
+        ChangePrizeEvent?.Invoke();
+    }
+}
+
+
---
+++ Assets/Scripts/Events/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance = null;
+    [SerializeField] private TextMeshProUGUI coinText;
+    [SerializeField] private TextMeshProUGUI earningPrizeText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        CoinManagerwithEvent.EarnMoneyEvent += UpdateCoinText;
+    }
+    private void UpdateCoinText()
+    {
+        coinText.text = CoinManagerwithEvent.instance.coin.ToString();
+    }
+    public void UpdatePrizeText()
+    {
+        earningPrizeText.text = "Earn " + CoinManagerwithEvent.instance.earningPrize + " Coin";
+    }
+   
+}
