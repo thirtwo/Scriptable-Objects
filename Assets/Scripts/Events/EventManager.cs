7de8770f097e9e2d7c23825fe74a13279d9988ef
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-06-02 18:27:09 +0300

    Unity Events added

---
+++ Assets/Scripts/Events/EventManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+
+    [Serializable] public class OnSpacePressed : UnityEvent { }
+
+    public OnSpacePressed spacePressedEvent;
+
+    private void Start()
+    {
+        spacePressedEvent.AddListener(CoinManagerwithEvent.instance.EarnMoneyButton);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spacePressedEvent?.Invoke();
+        }
+    }
+}
